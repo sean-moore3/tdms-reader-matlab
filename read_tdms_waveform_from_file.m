1903c1af3c35ad12a8106fa11e708b808f3dc238
Sean Moore <sean.moore@ni.com>
2020-06-29 13:33:49 -0500

    create sample count attribute

--- read_tdms_waveform_from_file.m
+++ read_tdms_waveform_from_file.m
@@ -11,11 +11,12 @@ clear tdms_waveform % free up some space
 complex_waveform.real = interleaved_iq(1:2:end);
 complex_waveform.imaginary = interleaved_iq(2:2:end);
 complex_waveform.iq = complex_waveform.real + 1i .* complex_waveform.imaginary;
+complex_waveform.sample_count = length(complex_waveform.iq);
 clear interleaved_iq % free up some space
 
 % scan and fill metadata
 complex_waveform.burst_start_locations = 1;
-complex_waveform.burst_stop_locations = length(complex_waveform.iq);
+complex_waveform.burst_stop_locations = complex_waveform.sample_count;
 for i = 1:length(channel_property_names)
     property_name = channel_property_names{i};
     property_value = channel_property_values{i};
@@ -44,7 +45,7 @@ end
 clear channel_property_names channel_property_values property_name property_value
 
 % build burst mask
-complex_waveform.burst_mask = false(1, length(complex_waveform.iq));
+complex_waveform.burst_mask = false(1, complex_waveform.sample_count);
 for i = 1:length(complex_waveform.burst_start_locations)
     burst_start_location = complex_waveform.burst_start_locations(i);
     burst_stop_location = complex_waveform.burst_stop_locations(i);
@@ -53,7 +54,7 @@ end
 clear burst_start_location burst_stop_location
 
 % perform some sanity checks
-fprintf('Waveform Length (s): %.3f\n', complex_waveform.dt * length(complex_waveform.iq));
+fprintf('Waveform Length (s): %.3f\n', complex_waveform.dt * complex_waveform.sample_count);
 simulated_rms_power = 10 * log10(mean(...
     complex_waveform.real(complex_waveform.burst_mask).^2 + ...
     complex_waveform.imaginary(complex_waveform.burst_mask).^2)); 
@@ -62,7 +63,7 @@ fprintf('Calculated Pavg (dBFS): %.3f\n', simulated_rms_power);
 
 % finish off with some additional traces
 power_trace = 10 * log10(complex_waveform.real.^2 + complex_waveform.imaginary.^2); 
-time = 0:complex_waveform.dt:complex_waveform.dt*(length(complex_waveform.iq)-1);
+% time = 0:complex_waveform.dt:complex_waveform.dt*(complex_waveform.sample_count-1);
 % plot(time, power_trace);
 % xlabel('Time (s)');
 % ylabel('Power (dBFS)');
